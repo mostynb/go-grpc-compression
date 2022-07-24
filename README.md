13cc97e840bbf1af0c5f94793ef284263bd8b602
Mostyn Bramley-Moore <mostyn@antipode.se>
2022-07-24 15:15:17 +0200

    Add experimental/klauspost_snappy package, which might be more efficient than the google snappy package
    Relates to #10.

--- README.md
+++ README.md
@@ -3,12 +3,14 @@
 This respository contains go gRPC encoding wrappers for some useful compression
 algorithms that are not available in google.golang.org/grpc.
 
-* https://github.com/golang/snappy
-* https://github.com/klauspost/compress/tree/master/zstd
-* https://github.com/pierrec/lz4
+* snappy - using https://github.com/golang/snappy
+* zstd - using https://github.com/klauspost/compress/tree/master/zstd
+* lz4 - using https://github.com/pierrec/lz4
 
 The following algorithms also have experimental implementations, which have
 not been tested as much as those above. These may be changed significantly, or
 even removed from this library at a future point.
 
-* https://github.com/klauspost/compress/tree/master/s2
+* experimental/s2 - using https://github.com/klauspost/compress/tree/master/s2
+* experimental/klauspost_snappy - using https://github.com/klauspost/compress/tree/master/s2
+  in snappy compatibility mode
---
+++ experimental/klauspost_snappy/klauspost_snappy_test.go
@@ -0,0 +1,100 @@
+/*
+ *
+ * Copyright 2021 gRPC authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package klauspost_snappy
+
+import (
+	"bytes"
+	"context"
+	"io/ioutil"
+	"net"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/encoding"
+	"google.golang.org/grpc/test/bufconn"
+
+	"github.com/mostynb/go-grpc-compression/internal/testserver"
+)
+
+const (
+	bufSize = 1024
+	message = "Message Request snappy"
+)
+
+func TestRegisteredCompression(t *testing.T) {
+	comp := encoding.GetCompressor(Name)
+	require.NotNil(t, comp)
+	assert.Equal(t, Name, comp.Name())
+
+	buf := bytes.NewBuffer(make([]byte, 0, bufSize))
+	wc, err := comp.Compress(buf)
+	require.NoError(t, err)
+
+	_, err = wc.Write([]byte(message))
+	require.NoError(t, err)
+	assert.NoError(t, wc.Close())
+
+	r, err := comp.Decompress(buf)
+	require.NoError(t, err)
+	expected, err := ioutil.ReadAll(r)
+	require.NoError(t, err)
+
+	assert.Equal(t, message, string(expected))
+}
+
+func TestRoundTrip(t *testing.T) {
+	lis := bufconn.Listen(bufSize)
+	t.Cleanup(func() {
+		assert.NoError(t, lis.Close())
+	})
+
+	done := make(chan struct{}, 1)
+
+	s := grpc.NewServer()
+	defer func() {
+		s.GracefulStop()
+		<-done
+	}()
+	testserver.RegisterTestServerServer(s, &testserver.EchoTestServer{})
+	go func() {
+		if err := s.Serve(lis); err != nil && err != grpc.ErrServerStopped {
+			t.Errorf("Server exited with error: %v", err)
+		}
+		done <- struct{}{}
+	}()
+
+	ctx := context.Background()
+	conn, err := grpc.DialContext(ctx, "bufnet",
+		grpc.WithContextDialer(func(context.Context, string) (net.Conn, error) {
+			return lis.Dial()
+		}),
+		grpc.WithDefaultCallOptions(grpc.UseCompressor(Name)),
+		grpc.WithInsecure())
+	require.NoError(t, err)
+	t.Cleanup(func() {
+		assert.NoError(t, conn.Close())
+	})
+
+	client := testserver.NewTestServerClient(conn)
+	resp, err := client.SendMessage(ctx, &testserver.MessageRequest{Request: message})
+	require.NoError(t, err)
+	assert.Equal(t, message, resp.Response)
+}
