25439583c3123b8f4a2f0d550c11b9598adc11f0
Jacob Aronoff <jacobaronoff45@gmail.com>
2023-06-08 11:51:32 -0400

    Set friendlier zstd options

--- zstd/zstd.go
+++ zstd/zstd.go
@@ -27,14 +27,30 @@ import (
 
 const Name = "zstd"
 
+var encoderOptions = []zstd.EOption{
+	// The default zstd window size is 8MB, which is much larger than the
+	// typical RPC message and wastes a bunch of memory.
+	zstd.WithWindowSize(512 * 1024),
+	// By default zstd uses GOMAXPROCS (64 in prod) threads to encode a
+	// given input, which is overkill for a typical RPC message and
+	// further wastes memory.
+	zstd.WithEncoderConcurrency(1),
+}
+
+var decoderOptions = []zstd.DOption{
+	// Similar, zstd uses GOMAXPROCS threads for decoding, which is
+	// overkill.
+	zstd.WithDecoderConcurrency(1),
+}
+
 type compressor struct {
 	encoder *zstd.Encoder
 	decoder *zstd.Decoder
 }
 
 func init() {
-	enc, _ := zstd.NewWriter(nil)
-	dec, _ := zstd.NewReader(nil)
+	enc, _ := zstd.NewWriter(nil, encoderOptions...)
+	dec, _ := zstd.NewReader(nil, decoderOptions...)
 	c := &compressor{
 		encoder: enc,
 		decoder: dec,
